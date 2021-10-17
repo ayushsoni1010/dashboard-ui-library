a25b0f72b523e8825163e21e95deb969518ec65e
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-17 09:58:56 +0530

    🚀Feat: Added Header (#5)

---
+++ financial-analysis-ui/src/components/section-components/Header/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './style.header.css';
+import logoIcon from '../../../assets/logoIcon.svg';
+import squareIcon from "../../../assets/squareIcon.svg";
+import optionIcon from "../../../assets/optionIcon.svg";
+import graphIcon from "../../../assets/graphIcon.svg";
+import bellIcon from "../../../assets/bellIcon.svg";
+import avatar from "../../../assets/avatar.png";
+
+export default function Header() {
+    return (
+      <header className="header">
+        <div className="logo-header-wrapper">
+          <a href="/">
+            <button className="logo-icon-wrapper">
+              <img src={logoIcon} alt="Logo-Icon" />
+            </button>
+            <p className="logo-header-title">ECLIPSE</p>
+          </a>
+        </div>
+        <div className="header-options-wrapper">
+          <a href="/">
+            <button className="btn-square-icon">
+              <img src={squareIcon} alt="Sqaure-Icon" />
+            </button>
+          </a>
+          <a href="/">
+            <button className="btn-graph-icon">
+              <img src={graphIcon} alt="Graph-Icon" />
+            </button>
+          </a>
+          <a href="/">
+            <button className="btn-bell-icon">
+              <img src={bellIcon} alt="Bell-Icon" />
+            </button>
+          </a>
+          <a href="/">
+            <button className="btn-avatar-icon">
+              <img src={avatar} alt="Profile-Icon" />
+            </button>
+          </a>
+          <a href="/">
+            <button className="btn-option-icon">
+              <img src={optionIcon} alt="Option-Icon" />
+            </button>
+          </a>
+        </div>
+      </header>
+    );
+}
