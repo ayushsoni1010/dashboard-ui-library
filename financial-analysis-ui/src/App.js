83c11431b69cc41d5d5f1ed44e8a2cdcc873b378
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-18 21:05:50 +0530

    🚀Feat: Added categories component (#11)

--- financial-analysis-ui/src/App.js
+++ financial-analysis-ui/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './styles/index.css';
 import './App.css';
 import Header from './components/section-components/Header';
-import NavbarOptions from './components/section-components/NavbarOptions';
+import Categories from './components/section-components/Categories';
+// import NavbarOptions from './components/section-components/NavbarOptions';
 
 function App() {
   return (
     <div className="">
       <Header />
-      <NavbarOptions/>
+      <Categories/>
     </div>
   );
 }
