563ff1763bc70c6bd38b74f1f6d662da7a96e732
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-18 15:58:58 +0530

    🚀Update: header & buttons

--- financial-analysis-ui/src/components/widgets/Buttons/SecondaryButton/index.js
+++ financial-analysis-ui/src/components/widgets/Buttons/SecondaryButton/index.js
@@ -1,27 +1,48 @@
 import React from "react";
-import './style.buttons.secondaryButton.css';
-import '../../../../styles/index.css';
+import "./style.buttons.secondaryButton.css";
+import "../../../../styles/index.css";
 
 export default function SecondaryButton(__secondaryButonProperties) {
-    if (!__secondaryButonProperties.startIcon && !__secondaryButonProperties.imageIcon) {
-      return (
-        <button className="secondary-button-properties">
-          {__secondaryButonProperties.value}
-        </button>
-      );
-    } else if(!__secondaryButonProperties.imageIcon){
-      return (
-        <button className="secondary-button-icon-properties">
-          <i className={__secondaryButonProperties.startIcon}></i>
-          {__secondaryButonProperties.value}
-        </button>
-      );
-  }
-    else {
-      return (
-        <button className="secondary-button-image-icon-properties">
-          <img src={__secondaryButonProperties.imageIcon} alt="icon"/>{__secondaryButonProperties.value}
-        </button>
-      )
+  if (
+    !__secondaryButonProperties.startIcon &&
+    !__secondaryButonProperties.imageIcon
+  ) {
+    return (
+      <button className="secondary-button-only-value-properties">
+        {__secondaryButonProperties.value}
+      </button>
+    );
+  } else if (
+    !__secondaryButonProperties.value &&
+    !__secondaryButonProperties.startIcon
+  ) {
+    return (
+      <button className="secondary-button-only-image-icon-properties">
+        <img src={__secondaryButonProperties.imageIcon} alt="Icon" />
+      </button>
+    );
+  } else if (
+    !__secondaryButonProperties.imageIcon &&
+    !__secondaryButonProperties.value
+  ) {
+    return (
+      <button className="secondary-button-only-start-icon-properties">
+        <i className={__secondaryButonProperties.startIcon}></i>
+      </button>
+    );
+  } else if (!__secondaryButonProperties.imageIcon) {
+    return (
+      <button className="secondary-button-start-icon-value-properties">
+        <i className={__secondaryButonProperties.startIcon}></i>
+        {__secondaryButonProperties.value}
+      </button>
+    );
+  } else {
+    return (
+      <button className="secondary-button-image-icon-value-properties">
+        <img src={__secondaryButonProperties.imageIcon} alt="icon" />
+        {__secondaryButonProperties.value}
+      </button>
+    );
   }
 }
