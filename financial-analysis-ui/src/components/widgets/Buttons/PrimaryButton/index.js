528fa9c3444d5069426e525944d9fc505aac2373
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-21 18:03:45 +0530

    🚀Feat: Added Primary button (#22)

--- financial-analysis-ui/src/components/widgets/Buttons/PrimaryButton/index.js
+++ financial-analysis-ui/src/components/widgets/Buttons/PrimaryButton/index.js
@@ -1,23 +1,11 @@
 import React from "react";
-import "./style.buttons.primaryButton.css";
+import './style.buttons.primaryButton.css';
 
 export default function PrimaryButton(__primaryButtonProperties) {
-  if (!__primaryButtonProperties.startIcon) {
     return (
-      <div className="primary-button-properties-only-value">
-        <button>{__primaryButtonProperties.value}</button>
-        <span>{__primaryButtonProperties.stats}</span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="primary-button-properties-start-icon-value">
-        <button className={__primaryButtonProperties.type}>
-          <i className={__primaryButtonProperties.startIcon}></i>
-        </button>
-        <p>{__primaryButtonProperties.value}</p>
-        <span>{__primaryButtonProperties.stats}</span>
-      </div>
-    );
-  }
+        <div className="primary-button-wrapper">
+            <button className="primary-button-icon-wrapper"><i className={__primaryButtonProperties.startIcon}></i></button>
+            <p className="primary-button-title">{__primaryButtonProperties.title}</p>
+        </div>
+    )
 }
