90e81b6318c171653fc64ea039c27e067918198a
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-21 22:24:26 +0530

    🚀Update: header and cards styling

--- financial-analysis-ui/src/App.js
+++ financial-analysis-ui/src/App.js
@@ -2,47 +2,22 @@ import React from "react";
 import "./styles/index.css";
 import "./App.css";
 import Header from "./components/section-components/Header";
-// import IncomeCard from "./components/widgets/Cards/IncomeCard";
-// import Categories from './components/section-components/Categories';
-// import NavbarOptions from './components/section-components/NavbarOptions';
+import Home from "./views/Home";
+import Navbar from "./views/Navbar";
+import Sidebar from "./views/Sidebar";
 
 function App() {
   return (
-    <div className="">
+    <div className="center_view">
       <Header />
 
-
-
-
-
-      {/* <IncomeCard
-        startIcon="fas fa-download"
-        title="Income"
-        stats="$27,632"
-        endIcon=" fas fa-ellipsis-h"
-        type="orange"
-      />
-      <IncomeCard
-        startIcon="fas fa-hand-holding-usd"
-        title="Expense"
-        stats="$8,632"
-        endIcon=" fas fa-ellipsis-h"
-        type="green"
-      />
-      <IncomeCard
-        startIcon="fas fa-comment-dollar"
-        title="Cashback"
-        stats="$12,809"
-        endIcon=" fas fa-ellipsis-h"
-        type="blue"
-      />
-      <IncomeCard
-        startIcon="fas fa-coffee"
-        title="Offer"
-        stats="$27,632"
-        endIcon=" fas fa-ellipsis-h"
-        type="red"
-      /> */}
+      <div className="center_view landscape_view">
+        <Navbar />
+        <div className="marginal_center_view">
+          <Home />
+        </div>
+        <Sidebar />
+      </div>
     </div>
   );
 }
