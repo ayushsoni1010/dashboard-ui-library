75bc7120cef97180fb58d3e734d8747c727b1f62
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-21 21:26:01 +0530

    🚀Update: Categories component (#11)

--- financial-analysis-ui/src/components/section-components/Categories/index.js
+++ financial-analysis-ui/src/components/section-components/Categories/index.js
@@ -1,35 +1,35 @@
 import React from "react";
-import PrimaryButton from "../../widgets/Buttons/PrimaryButton";
+import CategoryButton from "../../widgets/Buttons/CategoryButton";
 import './style.categories.css';
 
 export default function Categories() {
     return (
       <div className="categories-options-wrapper">
-        <PrimaryButton
+        <CategoryButton
           type="btn-bg-blue"
           startIcon="fas fa-tshirt"
           value="Clothes & Fashion"
           stats="$5540"
         />
-        <PrimaryButton
+        <CategoryButton
           type="btn-bg-red"
           startIcon="fas fa-utensils"
           value="Restaurants"
           stats="$5540"
         />
-        <PrimaryButton
+        <CategoryButton
           type="btn-bg-green"
           startIcon="fas fa-theater-masks"
           value="Cinema & Theaters"
           stats="$1290"
         />
-        <PrimaryButton
+        <CategoryButton
           type="btn-bg-light-blue"
           startIcon="fas fa-briefcase-medical"
           value="Medications"
           stats="$1290"
         />
-        <PrimaryButton
+        <CategoryButton
           type="btn-bg-orange"
           startIcon="fas fa-home"
           value="House"
